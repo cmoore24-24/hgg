f9a1c9313769296564112ae3a87a9be25a812494
Connor Moore <cmoore24@condorfe.crc.nd.edu>
2024-03-20 18:55:14 -0400

    Averaged Color Ring

--- processors/cr_investigator_average_cands.py
+++ processors/cr_investigator_average_cands.py
@@ -78,18 +78,19 @@ if __name__ == "__main__":
             metadata={"dataset": info["label"]},
         ).events()
 
-    def num_subjets(fatjet, cluster):
+    def num_subjets(fatjet, cluster_val):
         pf = ak.flatten(fatjet.constituents.pf, axis=1)
-        jetdef = fastjet.JetDefinition(fastjet.cambridge_algorithm, cluster)
+        jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, cluster_val)
         cluster = fastjet.ClusterSequence(pf, jetdef)
         subjets = cluster.inclusive_jets()
         num = ak.num(subjets, axis=1)
         return num
 
-    def color_ring(fatjet, cluster, variant=False):
+    def color_ring(fatjet, cluster_val):
         pf = ak.flatten(fatjet.constituents.pf, axis=1)
-        jetdef = fastjet.JetDefinition(fastjet.cambridge_algorithm, cluster)
+        jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, cluster_val)
         cluster = fastjet.ClusterSequence(pf, jetdef)
+        #subjets = cluster.exclusive_jets(n_jets=3)
         subjets = cluster.inclusive_jets()
         vec = ak.zip(
             {
@@ -104,41 +105,34 @@ if __name__ == "__main__":
         vec = ak.pad_none(vec, 3)
         vec["norm3"] = np.sqrt(vec.dot(vec))
         vec["idx"] = ak.local_index(vec)
-        i, j, k = ak.unzip(ak.combinations(vec, 3))
-        #best = ak.argmin(abs((i + j + k).mass - 125), axis=1, keepdims=True)
-        best = ak.argmax(abs((i + j + k).mass), axis=1, keepdims=True)
-        order_check = ak.concatenate([i[best].pt, j[best].pt, k[best].pt], axis=1)
-        largest = ak.argmax(order_check, axis=1, keepdims=True)
-        smallest = ak.argmin(order_check, axis=1, keepdims=True)
-        leading_particles = ak.concatenate([i[best], j[best], k[best]], axis=1)
-        leg1 = leading_particles[largest]
-        leg3 = leading_particles[smallest]
-        leg2 = leading_particles[
-            (leading_particles.idx != ak.flatten(leg1.idx))
-            & (leading_particles.idx != ak.flatten(leg3.idx))
-        ]
+        i, j = ak.unzip(ak.combinations(vec, 2))
+        best = ak.argmax(abs((i + j).pt), axis=1, keepdims=True)
+        order_check = ak.concatenate([i[best].pt, j[best].pt], axis=1)
+        leading = ak.argmax(order_check, axis=1, keepdims=True)
+        subleading = ak.argmin(order_check, axis=1, keepdims=True)
+        leading_particles = ak.concatenate([i[best], j[best]], axis=1)
+        cut = ((vec.idx != ak.firsts(leading_particles.idx)) & 
+           (vec.idx != ak.firsts(ak.sort(leading_particles.idx, ascending=False)))
+          )
+        everything_else = vec[cut]
+        total_pt = ak.sum(vec.pt, axis=1)
+        everything_else['momentum_fraction'] = (everything_else.pt)/total_pt
+        everything_else['weighted_eta'] = everything_else.eta * everything_else.momentum_fraction
+        everything_else['weighted_phi'] = everything_else.phi * everything_else.momentum_fraction
+        weighted_average_eta = ak.sum(everything_else.weighted_eta, axis=1)/ak.num(everything_else, axis=1)
+        #average_eta = ak.sum(everything_else.eta, axis=1)/ak.num(everything_else, axis=1)
+        weighted_average_phi = ak.sum(everything_else.weighted_phi, axis=1)/ak.num(everything_else, axis=1)
+        #average_phi = ak.sum(everything_else.phi, axis=1)/ak.num(everything_else, axis=1)
+        leg1 = leading_particles[leading]
+        leg2 = leading_particles[subleading]
         leg1 = ak.firsts(leg1)
         leg2 = ak.firsts(leg2)
-        leg3 = ak.firsts(leg3)
-        a12 = np.arccos(leg1.dot(leg2) / (leg1.norm3 * leg2.norm3))
-        a13 = np.arccos(leg1.dot(leg3) / (leg1.norm3 * leg3.norm3))
-        a23 = np.arccos(leg2.dot(leg3) / (leg2.norm3 * leg3.norm3))
-        if not variant:
-            color_ring = (a13**2 + a23**2) / (a12**2)
-        else:
-            color_ring = a13**2 + a23**2 - a12**2
+        a13 = ((((leg1.eta * (leg1.pt/total_pt)) - weighted_average_eta)**2) + (((leg1.phi * (leg1.pt/total_pt)) - weighted_average_phi)**2))
+        a23 = ((((leg2.eta * (leg2.pt/total_pt)) - weighted_average_eta)**2) + (((leg2.phi * (leg2.pt/total_pt)) - weighted_average_phi)**2))
+        a12 = ((((leg1.eta * (leg1.pt/total_pt)) - (leg2.eta * (leg2.pt/total_pt)))**2) + (((leg1.phi * (leg1.pt/total_pt)) - (leg2.phi * (leg2.pt/total_pt)))**2))
+        color_ring = (a13 + a23) / (a12)
+    
         return color_ring
-
-    def d2_calc(fatjet):
-        jetdef = fastjet.JetDefinition(
-            fastjet.cambridge_algorithm, 0.8
-        )
-        pf = ak.flatten(fatjet.constituents.pf, axis=1)
-        cluster = fastjet.ClusterSequence(pf, jetdef)
-        softdrop = cluster.exclusive_jets_softdrop_grooming()
-        softdrop_cluster = fastjet.ClusterSequence(softdrop.constituents, jetdef)
-        d2 = softdrop_cluster.exclusive_jets_energy_correlator(func="D2")
-        return d2
 
     class MyProcessor(processor.ProcessorABC):
         def __init__(self):
@@ -186,7 +180,7 @@ if __name__ == "__main__":
                 * boosted_fatjet.constituents.pf.puppiWeight
             )
 
-            num_sub = ak.unflatten(num_subjets(boosted_fatjet, cluster=0.2), counts=ak.num(boosted_fatjet))
+            num_sub = ak.unflatten(num_subjets(boosted_fatjet, cluster_val=0.4), counts=ak.num(boosted_fatjet))
             boosted_fatjet['num_subjets'] = num_sub
             sub_cut = (boosted_fatjet.num_subjets >= 3)
             boosted_fatjet = boosted_fatjet[sub_cut]
@@ -206,10 +200,10 @@ if __name__ == "__main__":
 
             if "Color_Ring" in enabled_functions:
                 uf_cr = ak.unflatten(
-                     color_ring(boosted_fatjet, cluster=0.2), counts=ak.num(boosted_fatjet)
+                     color_ring(boosted_fatjet, cluster_val=0.4), counts=ak.num(boosted_fatjet)
                 )
                 boosted_fatjet["color_ring"] = uf_cr
-                fill_cr = ak.fill_none(ak.flatten(boosted_fatjet.color_ring), 0)
+                fill_cr = ak.fill_none(ak.flatten(boosted_fatjet.color_ring), 10)
                 multi_axis = (
                     dhist.Hist.new
                     .Reg(40, 0, 3, name="Color_Ring", label="Color_Ring", overflow=False, underflow=False)
@@ -217,7 +211,8 @@ if __name__ == "__main__":
                     .Reg(40, 50, 150, name="Mass", label="Mass", overflow=False, underflow=False)
                     .Reg(40, 50, 150, name="SDMass", label="SDMass", overflow=False, underflow=False)
                     .Weight()
-                    .fill(Color_Ring=fill_cr, 
+                    .fill(
+                          Color_Ring=fill_cr, 
                           PT=ak.flatten(boosted_fatjet.pt),
                           Mass=ak.flatten(boosted_fatjet.mass),
                           SDMass=ak.flatten(boosted_fatjet.msoftdrop)
@@ -313,7 +308,7 @@ if __name__ == "__main__":
         lib_resources={'cores': 12, 'slots': 12},
     )
 
-    name = '../outputs/cr_investigations/multi_var_hists/cambridge02_pt_mass_argmax.pkl'
+    name = '../outputs/cr_investigations/multi_var_hists/antikt04_averaged.pkl'
     with open(name, 'wb') as f:
         pickle.dump(computed, f)
 
